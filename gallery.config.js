332f53d22cbe40902fb7a9c5015392d97b6d8882
Rehane <abusayid693@gmail.com>
2023-01-08 00:15:05 +0530

    feat: configs

---
+++ gallery.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // supporting image formats, default will be applied if empty   
+  formats: [],
+  // files and directory to ignore, default will be applied if empty
+  ignore: [],
+  // per page limit, default will be applied if null
+  pageLimit: 10
+};
