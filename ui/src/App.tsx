56b3bf40b1217466049ca8038ac29fd19981aed2
Rehane <abusayid693@gmail.com>
2023-01-08 00:22:53 +0530

    feat: added ui library

---
+++ ui/src/App.tsx
@@ -0,0 +1,54 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import * as io from "./utils/io";
+
+function App() {
+  const [images, setImages] = useState<any[]>([]);
+
+  useEffect(() => {
+    const socket = io.connectWithSocketIOServer();
+
+    socket.on("load-images", (data:any) => {
+      console.log("load-iames :", data);
+      setImages(data.d);
+    });
+  }, []);
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {images?.map((image:any, index) => {
+          if (image?.type === "svg") {
+            return (
+              <img
+              key={index}
+                src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                  image?.buffer
+                )}`}
+              />
+            );
+          }
+          else if(image?.type === "png"){
+            return (
+              <img key={index} src={`data:image/png;base64,${image?.buffer}`}/>
+            )
+          }
+        })}
+
+        <p>Hello</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/utils/io.ts
@@ -0,0 +1,14 @@
+import io, {Socket} from 'socket.io-client';
+const SERVER = 'http://localhost:5002';
+
+let socket: Socket<any, any>;
+
+export const connectWithSocketIOServer = () => {
+  socket = io(SERVER);
+
+  socket.on('connect', () => {
+    console.log(`socket io connected id: ${socket.id}`);
+  });
+
+  return socket;
+};
